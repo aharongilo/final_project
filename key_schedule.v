900b738e7bc2e00d359fb6e1fbb69dba65e44219
aharong <pinhas02@gmail.com>
2022-03-13 22:23:20 +0200

    create key schedule module. basic: input and output

---
+++ key_schedule.v
@@ -0,0 +1,29 @@
+/*************************
+Final project 2022 - ANUBIS algorithm on FPGA 
+Authors: Yosef Berger, Aharon Gilo
+
+Module name: key schedule
+
+Description: this module represent the key schedule in the ANUBIS algorithm
+
+input:
+- clk
+- clk_en
+- load key
+- key (128 bits)
+
+output:
+- round key
+**************************/
+module key_schedule(
+	input clk,
+	input clk_en,
+	input load_key,
+	input [127:0] key,
+	output [127:0] round_key
+);
+
+
+
+
+endmodule
